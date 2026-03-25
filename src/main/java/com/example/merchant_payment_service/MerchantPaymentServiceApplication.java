633402f1084bed6d23fdab99772637876238f863
FurqanShafiq11 <furqanshafiq11@gmail.com>
2026-03-25 14:16:06 +0500

    Initial commit: Merchant Service Spring Boot Project

---
+++ src/main/java/com/example/merchant_payment_service/MerchantPaymentServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.merchant_payment_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MerchantPaymentServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MerchantPaymentServiceApplication.class, args);
+	}
+
+}
